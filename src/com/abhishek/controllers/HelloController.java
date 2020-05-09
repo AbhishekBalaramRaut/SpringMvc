a42cb73bb92e96490fa08da10e970b5c6d294624
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-05-09 12:57:00 -0700

    Multiple actions and @PathVariable single as well as Map

--- src/com/abhishek/controllers/HelloController.java
+++ src/com/abhishek/controllers/HelloController.java
@@ -1,16 +1,29 @@
 package com.abhishek.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
  
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
+	@RequestMapping("/welcome/{name}")
+	public ModelAndView helloWorld(@PathVariable("name") String name) {
 		ModelAndView mvc = new ModelAndView("welcome");
-		mvc.addObject("message", "Raut App");
+		mvc.addObject("message", "Welcome "+name);
 		return mvc;
 	}
+	
+	@RequestMapping("/welcome/{country}/{name}")
+	public ModelAndView hello2(@PathVariable Map<String,String> pathVars) {
+		ModelAndView mvc = new ModelAndView("welcome");
+		String country = pathVars.get("country");
+		String name = pathVars.get("name");
+		mvc.addObject("message", "Welcome "+name+", you are from "+country);
+		return mvc;
+	}
+	
 }
