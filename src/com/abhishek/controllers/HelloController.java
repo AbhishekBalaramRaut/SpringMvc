b726b32a8b9b6d236f4624cd7d337c676e4af8af
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-05-09 12:50:37 -0700

    Using @Controller, @RequestMapping annotations

--- src/com/abhishek/controllers/HelloController.java
+++ src/com/abhishek/controllers/HelloController.java
@@ -1,22 +1,16 @@
 package com.abhishek.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-
-public class HelloController extends AbstractController{
-
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
-
-		ModelAndView model = new ModelAndView("welcome");
-		model.addObject("message","Raut App");
-		return model;
+@Controller
+public class HelloController {
+ 
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+		ModelAndView mvc = new ModelAndView("welcome");
+		mvc.addObject("message", "Raut App");
+		return mvc;
 	}
 }
